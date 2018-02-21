2796c3382bda90e6596685fd1d4e9bf8f7c703f2
Robin Van den Broeck <robin.broeck@gmail.com>
2018-02-21 12:23:55 +0100

    Removed DisplayNames for LEAN

--- spring-mvc/src/main/java/be/ucll/runetracker/domain/User.java
+++ spring-mvc/src/main/java/be/ucll/runetracker/domain/User.java
@@ -1,11 +1,8 @@
 package be.ucll.runetracker.domain;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class User {
     private String email;
-    private List<DisplayName> displayNames = new ArrayList<>();
+    private String displayName;
 
     private User() {
         this(null);
@@ -23,22 +20,7 @@ public class User {
         return email;
     }
 
-    public void addDisplayName(DisplayName displayName) {
-        displayNames.add(displayName);
-    }
-
-    public Collection<DisplayName> getDisplayNames() {
-        return displayNames;
-    }
-
-    public Optional<DisplayName> getCurrentDisplayName() {
-        var sorted = sortedDisplayNames();
-        return Optional.ofNullable(sorted.get(sorted.size() - 1));
-    }
-
-    private List<DisplayName> sortedDisplayNames() {
-        return displayNames.stream()
-                .sorted(Comparator.comparing(DisplayName::getCreationDateTime))
-                .collect(Collectors.toUnmodifiableList());
+    public String setDisplayName(String displayName) {
+        this.displayName = displayName;
     }
 }
