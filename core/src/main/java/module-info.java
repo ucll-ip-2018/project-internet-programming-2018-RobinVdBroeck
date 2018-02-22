c192efb45b47a3c972602488c0f98354139bd12b
Robin Van den Broeck <robin.broeck@gmail.com>
2018-02-22 18:57:13 +0100

    Use Java9 modules

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module runetracker.core {
+    exports be.ucll.runetracker.database;
+    exports be.ucll.runetracker.domain;
+}
---
+++ web/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module runetracker.web {
+    requires spring.context;
+    requires spring.webmvc;
+    requires runetracker.core;
+}
