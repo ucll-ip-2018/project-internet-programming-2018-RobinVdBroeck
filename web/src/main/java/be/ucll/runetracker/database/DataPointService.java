b6254867f6dfab8fac03b70cea5b456cd9af8ea4
Robin Van den Broeck <robin.broeck@gmail.com>
2018-02-28 12:20:03 +0100

    Added more methods to the facade

--- web/src/main/java/be/ucll/runetracker/database/DataPointService.java
+++ web/src/main/java/be/ucll/runetracker/database/DataPointService.java
@@ -1,21 +1,25 @@
 package be.ucll.runetracker.database;
 
 import be.ucll.runetracker.domain.DataPoint;
+import be.ucll.runetracker.domain.Skill;
 import be.ucll.runetracker.domain.User;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public class DataPointService {
     private UserDatabase userDatabase;
     private DataPointDatabase dataPointDatabase;
+    private SkillDatabase skillDatabase;
 
     private DataPointService() {
-
     }
 
-    public DataPointService(UserDatabase userDatabase, DataPointDatabase dataPointDatabase) {
+    public DataPointService(UserDatabase userDatabase, DataPointDatabase dataPointDatabase, SkillDatabase skillDatabase) {
         setUserDatabase(userDatabase);
         setDataPointDatabase(dataPointDatabase);
+        setSkillDatabase(skillDatabase);
     }
 
     private UserDatabase getUserDatabase() {
@@ -34,6 +38,18 @@ public class DataPointService {
         this.dataPointDatabase = dataPointDatabase;
     }
 
+    private SkillDatabase getSkillDatabase() {
+        return skillDatabase;
+    }
+
+    private void setSkillDatabase(SkillDatabase skillDatabase) {
+        this.skillDatabase = skillDatabase;
+    }
+
+    // USERS
+    public Optional<User> getUser(String email) {
+        return userDatabase.get(email);
+    }
 
     public List<User> getAllUsers() {
         return userDatabase.all();
@@ -43,7 +59,42 @@ public class DataPointService {
         userDatabase.add(user);
     }
 
+
+    public void deleteUser(User user) {
+        userDatabase.delete(user);
+    }
+
+    // DATAPOINTS
+    public Optional<DataPoint> getDatapoint(int id) {
+        return dataPointDatabase.get(id);
+    }
+
     public List<DataPoint> getAllDatapoints() {
         return dataPointDatabase.all();
+    }
+
+    public void addDatapoint(DataPoint dataPoint) {
+        dataPointDatabase.add(dataPoint);
+    }
+
+    public void deletedDataPoint(DataPoint dataPoint) {
+        dataPointDatabase.delete(dataPoint);
+    }
+
+    // Skills
+    public Optional<Skill> getSkill(String name) {
+        return skillDatabase.get(name);
+    }
+
+    public List<Skill> getAllSkills() {
+        return skillDatabase.all();
+    }
+
+    public void addSkill(Skill skill) {
+        skillDatabase.add(skill);
+    }
+
+    public void deleteSkill(Skill skill) {
+        skillDatabase.delete(skill);
     }
 }
